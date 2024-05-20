053de36a41dee92a2980ae8d65bb54f74a1daa40
Bruno Araujo <brnasc.work@proton.me>
2024-05-19 21:18:46 -0300

    change background and color style

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,21 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello Flutter'),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                colors: [Colors.blueGrey, Color.fromARGB(255, 57, 68, 74)],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight),
+          ),
+          child: const Center(
+            child: Text(
+              'Hello Flutter',
+              style: TextStyle(fontSize: 28, color: Colors.white),
+            ),
+          ),
         ),
       ),
     ),
